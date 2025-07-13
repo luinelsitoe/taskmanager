1fa48002f81c0e738fb66ec04effcbb0da459617
luinelsitoe <luinelsitoe@gmail.com>
2025-07-13 12:19:02 +0200

    Small changes

--- taskmanager/src/main/java/com/luinel/taskmanager/repository/TaskRepository.java
+++ taskmanager/src/main/java/com/luinel/taskmanager/repository/TaskRepository.java
@@ -4,10 +4,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.luinel.taskmanager.model.Status;
 import com.luinel.taskmanager.model.Task;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
   List<Task> findByTitleContaining(String title);
 
